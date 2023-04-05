1e99973cecf3e6a53118cbaeaec8ff7b9220df5b
tbetancur <tbetancur@unal.edu.co>
2023-04-05 17:01:53 -0500

    Solución Lab4
    Lab4 Solucionada

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,42 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_csv('clusters_report.txt', widths=[9,16,16,80], header=None)
+    
+    list_column = df[:2].fillna('').apply(lambda x:' '+x). sum().tolist()
+    list_column=[colum.strip().lower().replace(' ','_')for colum in list_column]
+    
+    df =df[3:]
+    df.columns = list_column
+    
+    df= df.fillna(method= 'ffill')
+    df.principales_palabras_claves = df.principales_palabras_claves.apply(
+        lambda words: ' ' + words
+    )
+    
+    df= df.groupby([
+        'cluster',
+        'cantidad_de_palabras_clave',
+        'porcentaje_de_palabras_clave'        
+    ], as_index =False)
+    [['principales_palabras_claves']].sum()
+    
+    df.principales_palabras_claves =df.principales_palabras_claves.str.replace(".","",regex=True)
+    df.principales_palabras_claves =df.principales_palabras_claves.str.replace ("  "," ")
+    df.principales_palabras_claves =df.principales_palabras_claves.str.replace (" "," ")
+    df.principales_palabras_claves =df.principales_palabras_claves.str.replace (" "," ")
+    df.principales_palabras_claves =df.principales_palabras_claves.str.strip()
+    
+    
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.replace('%', '')
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.replace(',', '.')
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.map(float)
 
+    df.cantidad_de_palabras_clave = df.cantidad_de_palabras_clave.map(int)
+    df.cluster = df.cluster.map(int)
+    df = df.sort_values('cluster')
+    df = df.reset_index(drop=True)
+    
     return df
